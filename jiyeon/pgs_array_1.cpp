7184fd72ac86b5732c1f5105ea76b64a1facbf85
jy6757 <143304578+jy6757@users.noreply.github.com>
2025-04-08 21:25:00 +0900

    Create pgs_array_1.cpp
    [week1][pgs]두 수 뽑아서 더하기 - jiyeon

---
+++ jiyeon/pgs_array_1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int numbers[], size_t numbers_len) {
+    int* answer = (int*)malloc(sizeof(int));
+    return answer;
+}
